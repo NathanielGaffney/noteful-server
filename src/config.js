c3140e73b2fb7293bd7bf8017b33d97b0d74b4dc
NathanielGaffney <nate440@gmail.com>
2020-10-02 19:59:12 -0400

    deployment changes

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DATABASE_URL || 'postgres://kwoazxaqzjdxxz:c90fed8ba607d2838159248f8b81e13791bb74e5a533ecc47b40ef9502a5e939@ec2-204-236-228-169.compute-1.amazonaws.com:5432/d9i3572bhq3trs',
+    DATABASE_URL: process.env.DATABASE_URL || 'localhost',
 }
